20fa5a88e4493d744b68a085190941c329147d6a
Scott Parlane <sjp@canterburyairpatrol.org>
2021-06-09 18:53:37 +1200

    check-code.sh: Add isort, improve pylint django

--- check-code.sh
+++ check-code.sh
@@ -2,9 +2,13 @@
 
 source venv/bin/activate
 
+PATHS="fss/ main/ config/ assets/"
+
+isort --check --diff ${PATHS}
+
 pycodestyle --ignore=E501 */*.py
 
-pylint --max-line-length=240 --load-plugins pylint_django fss/ main/ config/ assets/
+pylint --max-line-length=240 --load-plugins pylint_django --django-settings-module=fss.settings ${PATHS}
 
 deactivate
 
