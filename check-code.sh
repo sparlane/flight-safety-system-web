40c8b8377bc125ebc87fa2b51e48c611ee76d351
Scott Parlane <sjp@canterburyairpatrol.org>
2022-06-30 18:32:09 +1200

    check-code: Don't check migrations
    migrations are normally automatically generated, so there isn't much
    we can do about them.

--- check-code.sh
+++ check-code.sh
@@ -8,7 +8,7 @@ isort --check --diff --line-length 240 ${PATHS}
 
 pycodestyle --ignore=E501 */*.py
 
-pylint --max-line-length=240 --load-plugins pylint_django --django-settings-module=fss.settings ${PATHS}
+pylint --max-line-length=240 --load-plugins pylint_django --django-settings-module=fss.settings --ignore migrations ${PATHS}
 
 deactivate
 
