06ac8b3d58bbdd4190d7621734da3d0a35e44a69
Scott Parlane <scott@parlanenz.com>
2022-09-01 21:26:33 +1200

    checker: create pylintrc file and update check-code to use it
    Hold pylint at 2.14 to avoid an issue with it failing to load the django config

--- check-code.sh
+++ check-code.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 source venv/bin/activate
 
@@ -8,7 +8,7 @@ isort --check --diff --line-length 240 ${PATHS}
 
 pycodestyle --ignore=E501 */*.py
 
-pylint --max-line-length=240 --load-plugins pylint_django --django-settings-module=fss.settings --ignore migrations ${PATHS}
+pylint ${PATHS}
 
 deactivate
 
